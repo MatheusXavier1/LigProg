2609740c6e263bb3f6f70491725122eafa583bf1
MatheusXavier1 <matheusxavier.20241@poli.ufrj.br>
2024-09-11 00:00:08 -0300

    AULAS

---
+++ Aula1.cpp
@@ -0,0 +1,89 @@
+//software é uma solução intangível para um problema real
+
+//paradigmas: estruturado (orietado à objeto)
+
+//saber como e o que o "cliente" quer e transformar isso em um documento
+
+//"o que eu estou programando"--> antes de começar a programar
+
+//class em python
+
+//linguagem compilada: código é transformado pra um código objeto será lido pela máquina(faz diferença o hardware)
+
+//linguagem interpretada:
+
+//==================importante==============
+//o que é o software
+//paradigmas do software
+//fracamente/fortemente tipada
+//compilada/interpretada
+//ponteiro
+
+
+#include <stdio.h>
+int main(){
+    printf("oi");
+    return 0;
+}
+
+int soma(int a, int b){
+    return a + b;
+}
+//=========saber na semana==========
+//laço de repetição 
+//condicionais
+//funções
+
+//===========extra===========
+//switch case
+//struct
+
+//bolean não existe em C "int option = 0" 0 é false e qualquer coisa diferente de 0 e true
+
+//int a[10]; vetor com 10 espaços
+//int b[5][5]; vetor bidimensional 5x5
+
+//VARIAVEL SE DECLARA FORA DA "int main"
+
+for(int=i; i<5 ; i++){
+    printf(%d,"i")
+}
+
+//==função em C==
+
+//variavel nome-da-funcao (variavel/nome-do-parametro){ return a + b; }
+
+int soma (int a, int b) //promessa da declaração da função
+
+int main();
+
+soma(1,2)
+
+//============================
+//#include <stdio.h>
+
+int main(){
+
+    printf("Teste");
+    return 0;
+}
+
+#include <stdio.h>
+
+int a = 0;
+int b =1;
+
+ int soma (int primeiro, int segundo); // aqui é a promessa
+
+int main(){
+  int d = soma(a,b);
+    printf("%d\n,d");
+    return 0
+
+}
+int soma (int primeiro, int segundo){
+    return primeiro + segundo;
+}
+// https://github.com/coder/code-server VS CODE NO TABLET
+// VHEditor Android
+//GITHUB CODE SERVER
